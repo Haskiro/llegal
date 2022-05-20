cde97cc3c888e320336723a9020b42cdd64d6393
haspork <kondratev.pach@mail.ru>
2022-05-20 12:48:22 +0300

    burger fixed

--- sritpts/scripts.js
+++ sritpts/scripts.js
@@ -1,4 +1,4 @@
-const burger = document.querySelector(".burger");
+const burger = document.querySelector(".icons-list__button");
 const nav = document.querySelector(".nav")
 burger.addEventListener("click", () => {
     nav.classList.toggle("active");
